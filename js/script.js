fa31f90f4de9106e58079b859db634ce4f834995
afif <muhammadmufadholafif@gmail.com>
2026-01-05 14:45:33 +0700

    yey

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("Web Berhasil Dimuat");
+
+function loadquotes() {
+    console.log("Mengambil quote dari API");
+
+    fetch("https://v2.jokeapi.dev/joke/Any?type=single")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("quote").innerText = data.joke;
+            console.log("Quote berhasil dimuat");
+        })
+        .catch(error => {
+            document.getElementById("quote").innerText = "Gagal memuat quote";
+            console.error(error);
+        });
+}
